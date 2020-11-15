3d074c4227b1e85e83ea9ce627a9c0a39f459e57
andyPiersonCodes <71237914+andyPiersonCodes@users.noreply.github.com>
2020-11-15 12:52:47 -0500

    added test list in index.pug, added ID to ships.js

--- ships.js
+++ ships.js
@@ -1,4 +1,5 @@
 const ships = [{
+  id: 1,
   name: 'Nebulon B',
   type: 'Frigate',
   size: 300,
@@ -7,6 +8,7 @@ const ships = [{
   affiliations: ['Imperial', 'Rebels', 'Pirates'],
   isUnique: 'No'
 }, {
+  id: 2,
   name: 'Star Destroyer',
   type: 'Cruiser',
   size: 1600,
@@ -15,6 +17,7 @@ const ships = [{
   affiliations: ['Imperial'],
   isUnique: 'No'
 }, {
+  id: 3,
   name: 'TIE Fighter',
   type: 'Starfighter',
   size: 7,
@@ -23,6 +26,7 @@ const ships = [{
   affiliations: ['Imperial'],
   isUnique: 'No'
 }, {
+  id: 4,
   name: 'X-Wing',
   type: 'Starfighter',
   size: 13,
@@ -31,11 +35,12 @@ const ships = [{
   affiliations: ['Rebels'],
   isUnique: 'No'
 }, {
+  id: 5,
   name: 'Millennium Falcon',
   type: 'Freighter',
   size: 34,
   manufacturer: 'Corellian Engineering Corporation',
-  weapons: ['Quad Laser Cannons', 'Tractor Beams', 'Concussion Missile Launcher'],
+  weapons: ['Quad Laser Cannons', 'Tractor Beams', 'Concussion Missiles'],
   affiliations: ['Smugglers', 'Rebels', 'Hutt Cartel'],
   isUnique: 'Yes'
 }]
