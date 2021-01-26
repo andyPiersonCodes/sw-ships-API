7aabc75281ba200df0dbf79688334198fec89141
andyPiersonCodes <71237914+andyPiersonCodes@users.noreply.github.com>
2021-01-25 19:59:48 -0500

    added error page, and tested, added action file

--- pages/Error.jsx
+++ pages/Error.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import NotFound from '../components/NotFound'
 import Page from '../components/Page'
-import Title from '../components/Title'
 
 export default () => (
   <Page>
-    <Title />
-    <NotFound message="Sorry, unable to find the page you are looking for" />
+    <NotFound message="This was not the ship you were looking for" />
   </Page>
 )
