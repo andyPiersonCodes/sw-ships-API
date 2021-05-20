244066383f5815fed4313ce9488d7641e093366e
andyPiersonCodes <71237914+andyPiersonCodes@users.noreply.github.com>
2021-05-20 19:08:58 -0400

    added many to many relationships for ships and affiliations

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const allConfigs = require('../config/sequelize')
 const ShipsModel = require('./ships')
 const WeaponsModel = require('./weapons')
 const AffiliationsModel = require('./affiliations')
+const Ships_AffiliationsModel = require('./ships_Affiliations')
 
 const environment = process.env.NODE_ENV || 'development'
 const config = allConfigs[environment]
@@ -12,16 +13,17 @@ const connection = new Sequelize(config.database, config.username, config.passwo
 })
 
 const Ships = ShipsModel(connection, Sequelize)
+const Affiliations = AffiliationsModel(connection, Sequelize)
 const Weapons = WeaponsModel(connection, Sequelize, Ships)
-const Affiliations = AffiliationsModel(connection, Sequelize, Ships)
+const Ships_Affiliations = Ships_AffiliationsModel(connection, Sequelize, Ships, Affiliations)
 
 Weapons.belongsTo(Ships)
 Ships.hasMany(Weapons)
 
-Affiliations.belongsTo(Ships)
-Ships.hasMany(Affiliations)
+Ships.belongsToMany(Affiliations, { through: Ships_Affiliations })
+Affiliations.belongsToMany(Ships, { through: Ships_Affiliations })
 
-module.exports = { Ships, Weapons, Affiliations, Op: Sequelize.Op }
+module.exports = { Ships, Weapons, Affiliations, Ships_Affiliations, Op: Sequelize.Op }
 
 /*
 ENROLLMENTS IS THE LINKING TABLE
@@ -32,5 +34,8 @@ const Enrollments = EnrollmentsModel(connection, Sequelize, Members, Products)
 
 Employers.hasMany(Members)
 Members.belongsTo(Employers)
+
+Products.belongsToMany(Members, { through: Enrollments })
+Members.belongsToMany(Products, { through: Enrollments })
 
 */
